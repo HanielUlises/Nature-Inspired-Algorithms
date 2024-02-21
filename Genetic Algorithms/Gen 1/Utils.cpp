5592264c24bc09b637bd2f25df032548a9151035
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2024-02-20 18:14:42 -0600

    Optimizing a function

---
+++ Genetic Algorithms/Gen 1/Utils.cpp
@@ -0,0 +1,28 @@
+#include "Utils.h"
+
+#include <cstdlib>
+#include <stdexcept>
+#include <set>
+
+double randomDouble (){
+    return double(rand() % 1000)/1000;
+}
+
+bool randomProbability (double probability){
+    if(probability < 0 || probability > 1){
+        throw std::runtime_error("Probabilities are only between 0 and 1 \n");
+    }
+
+    double r = randomDouble();
+
+    return r < probability ? true : false;
+}
+
+double precision (int low, int high, int numberOfBits){
+    double prec = (double)(high - low)/(double)(pow(2,numberOfBits)-1);
+    return prec;
+}
+
+std::set<int> randomDistinctNumbers (int upperLimit, int amountNumbers){
+    
+}
