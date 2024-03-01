fd84f433d8cabee471ddedd0552090da4b8d6ed8
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2024-02-29 20:25:17 -0600

    Fitness selection

--- Optimization/main.cpp
+++ Optimization/main.cpp
@@ -2,19 +2,11 @@
 
 int main (){
     // Parte uno
-    std::vector<std::pair<double, double>> points_p1;
+    std::vector<std::pair<double, double>> points_p1 = {
+        {-5.5, 2.3}, {1.2, -1.1}, {6.0, 3.0}, {1.0, 0.9}, {-0.9, 0.8}, {2, 4.0}
+    };
 
-    points_p1.push_back(std::make_pair(-5.5,2.3));
-    points_p1.push_back(std::make_pair(1.2,-1.1));
-    points_p1.push_back(std::make_pair(6.0,3.0));
-    points_p1.push_back(std::make_pair(1.0,0.9));
-    points_p1.push_back(std::make_pair(-0.9,0.8));
-    points_p1.push_back(std::make_pair(2,4.0));
-
-    // Goal
-    std::vector<double> z;
-    // Expected value
-    std::vector<double> expected_z;
+    std::vector<double> z, expected_z;
 
     std::cout << "Points 1 tested" << std::endl;
     for (const auto& point : points_p1) {
@@ -22,15 +14,19 @@ int main (){
         f1(point.first, point.second, z);
     }
 
+    // Mean and expectancy values
     double mean_z = mean(z);
-
-    for(int i = 0; i < z.size(); i++){
-        expected_val(z[i], mean_z, expected_z);
+    for (double val : z) {
+        expected_val(val, mean_z, expected_z);
     }
 
-    std::cout<<std::endl<<"Expected values"<<std::endl;
-
-    for(double i: expected_z){
-        std::cout<< i<<std::endl;
+    // Display expected values
+    std::cout << std::endl << "Expected values" << std::endl;
+    for (double i : expected_z) {
+        std::cout << i << std::endl;
     }
+
+    // Select a point using roulette selection
+    auto& selectedPoint = rouletteSelection(points_p1, expected_z);
+    std::cout << std::endl << "Selected Point: (" << selectedPoint.first << ", " << selectedPoint.second << ")" << std::endl;
 }
