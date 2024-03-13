9d571d8ab6ebbafabd2acf9ad8e37b252675015a
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2024-03-12 19:03:44 -0600

    Genetic algorithm starters

--- Genetic Algorithms/Basics/Codification/GeneticAlgorithm.cpp
+++ Genetic Algorithms/Basics/Codification/GeneticAlgorithm.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <random>
 
+// Random number generator initialization
 std::random_device rd;
 std::mt19937 gen(rd());
 
@@ -16,8 +17,10 @@ GeneticAlgorithm::~GeneticAlgorithm() {
 
 void GeneticAlgorithm::run() {
     initializePopulation();
-    for (int gen = 0; gen < numberOfGenerations; ++gen) {
-        evaluateFitness();
+        for (int gen = 0; gen < numberOfGenerations; ++gen) {
+        // Functions test
+        evaluateFitness(std::bind(&GeneticAlgorithm::rosenbrockFunction, this, std::placeholders::_1));
+        // evaluateFitness(std::bind(&GeneticAlgorithm::ackleyFunction, this, std::placeholders::_1));
         auto selectedParents = selection();
         crossover(selectedParents);
         mutation();
@@ -28,8 +31,7 @@ void GeneticAlgorithm::run() {
 void GeneticAlgorithm::initializePopulation() {
 }
 
-void GeneticAlgorithm::evaluateFitness() {
-    // Evaluate the fitness of each individual in the population
+void GeneticAlgorithm::evaluateFitness(std::function<double(const std::vector<double>&)> objectiveFunction) {
     for (auto& individual : population) {
         double fitness = objectiveFunction(individual);
         fitnessValues.push_back(fitness);
@@ -50,6 +52,21 @@ bool GeneticAlgorithm::shouldStop(int currentGeneration) {
     return false; // Placeholder
 }
 
-double GeneticAlgorithm::objectiveFunction(const std::vector<double>& individual) {
-    return 0.0; // Placeholder
+double GeneticAlgorithm::rosenbrockFunction(const std::vector<double>& individual) {
+    double rosenbrock = 0.0;
+    for (size_t i = 0; i < individual.size() - 1; ++i) {
+        rosenbrock += 100 * std::pow((individual[i+1] - std::pow(individual[i], 2)), 2) + std::pow((1 - individual[i]), 2);
+    }
+    return rosenbrock;
+}
+
+double GeneticAlgorithm::ackleyFunction(const std::vector<double>& individual) {
+    double sum1 = 0.0;
+    double sum2 = 0.0;
+    for (auto x_i : individual) {
+        sum1 += std::pow(x_i, 2);
+        sum2 += std::cos(2 * M_PI * x_i);
+    }
+    double ackley = -20 * std::exp(-0.2 * std::sqrt(sum1 / individual.size())) - std::exp(sum2 / individual.size()) + 20 + M_E;
+    return ackley;
 }
