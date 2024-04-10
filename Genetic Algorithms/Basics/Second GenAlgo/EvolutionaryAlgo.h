4adc19a1409359b32d911700354e7004beeb0a39
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2024-04-09 20:23:56 -0600

    Jeje

---
+++ Genetic Algorithms/Basics/Second GenAlgo/EvolutionaryAlgo.h
@@ -0,0 +1,28 @@
+// EvolutionaryAlgorithm.h
+#ifndef EVOLUTIONARY_ALGORITHM_H
+#define EVOLUTIONARY_ALGORITHM_H
+
+#include <vector>
+
+class EvolutionaryAlgorithm {
+public:
+    EvolutionaryAlgorithm(int size, int populationSize, int generations);
+    void solve();
+
+private:
+    int size; // Size of the magic square
+    int populationSize; // Number of individuals in the population
+    int generations; // Number of generations to evolve
+    std::vector<std::vector<int>> population; // Population of solutions
+
+    void initializePopulation();
+    std::vector<int> generateIndividual();
+    int calculateFitness(const std::vector<int>& individual);
+    void selection();
+    void crossover(std::vector<int>& parent1, std::vector<int>& parent2);
+    void mutation(std::vector<int>& individual);
+    bool isMagicSquare(const std::vector<int>& square);
+    void printSolution(const std::vector<int>& solution);
+};
+
+#endif // EVOLUTIONARY_ALGORITHM_H
