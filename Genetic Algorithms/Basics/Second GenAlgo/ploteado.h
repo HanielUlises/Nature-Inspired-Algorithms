2888583225b2608d0ce7b96fe30308e5e88e0042
HanielUlises <hanielulises2003@gmail.com>
2024-04-13 18:54:36 -0600

    Convergence graph

--- Genetic Algorithms/Basics/Second GenAlgo/ploteado.h
+++ Genetic Algorithms/Basics/Second GenAlgo/ploteado.h
@@ -1,5 +1,8 @@
-#ifndef ploteado
-#define ploteado
+#ifndef PLOTEADO_H
+#define PLOTEADO_H
+
 #include <vector>
+
 void plotConvergenceGraph(std::vector<double> average, std::vector<double> best, std::vector<double> worst);
+
 #endif
