7d23c3bd0cb129e6a2a02b9e4f38d1128ef50b7d
Samuel-Sierra <bocalin@hotmail.com>
2024-04-26 09:28:19 -0600

    q ta chendo

--- Evolutionary Algorithms/Differential_Evolution.cpp
+++ Evolutionary Algorithms/Differential_Evolution.cpp
@@ -12,16 +12,31 @@ DifferentialEvolution::DifferentialEvolution(int population_size, int dimension,
     : population_size_(population_size), dimension_(dimension), generation_max_(generation_max), lower_bound_(lower_bound)
     , upper_bound_(upper_bound), F_(F), Cr_(Cr) {}
 
-void DifferentialEvolution::runEvolution(const std::string& strategy) {
-    std::cout << "Running Differential Evolution with strategy: " << strategy << std::endl;
+void DifferentialEvolution::runEvolution(const int select, const int recombi) {
     std::vector<double>bestFitnessHistory, worstFitnessHistory, averageFitnessHistory;
-    double best=9999;
+    double best=9999999999999999;
 
     initializePopulation();
     for (size_t g = 0; g < generation_max_; g++){
+        
+        for (size_t w = 0; w < population_size_; w++){
+            
+            double fitX=rosenbrockFunction(population[w].values);
+            if(fitX<best){
+                best = fitX;
+                bestx.values=population[w].values;
+                bestx.fitness=fitX;
+            }
+        }
+        for(auto x:bestx.values){
+            std::cout<<x<<", ";
+        }
+        std::cout<<std::endl;
+        std::cout<<"Fitness: "<<best<<std::endl;
+        std::cout<<"\n"<<std::endl;
+        
         for(size_t i = 0; i < population_size_;i++){
             std::vector<int> indexs = selection_r();
-            
             int r1=indexs[0];
             int r2=indexs[1];
             int r3=indexs[2];
@@ -32,41 +47,77 @@ void DifferentialEvolution::runEvolution(const std::string& strategy) {
             std::vector<double> child;
             std::uniform_real_distribution<double> distributionR(0.0,1.0);
 
-            for(size_t j = 0; j < dimension_ ;j++){
+            int cont=0;
+                
+            if(recombi==0){
+                for(size_t j = 0; j < dimension_ ;j++){
                 //bin
+                    double Crrandom = distributionR(random);
+                    if (Crrandom<=Cr_ || j==jrand){
+                        double x1;
+                        if(select==0){
+                            x1 = population[r1].values[j];
+                            
+                        }else if(select==1){
+                            x1 = bestx.values[j];
+                        }
+                        double x2 = population[r2].values[j];
+                        double x3 = population[r3].values[j];
+                        //se hace la muta con los index
+                        auto ui = population[r1].values[j]+F_*(population[r2].values[j]-population[r3].values[j]);
+                        child.push_back(ui); 
+                    }else{
+                        //agregas al hijo el padre
+                        child.push_back(population[i].values[j]);
+                    }
+                }
+            }else{
                 double Crrandom = distributionR(random);
-                if (Crrandom<=Cr_ || j==jrand){
-                    double x1 = population[r1].values[j];
+                int initj=0;
+                int j=initj;
+                while(Crrandom<=Cr_ && j<dimension_){        
+                    double x1;
+                    if(select==0){
+                        x1 = population[r1].values[j];         
+                    }else if(select==1){
+                        x1 = bestx.values[j];
+                    }
                     double x2 = population[r2].values[j];
                     double x3 = population[r3].values[j];
                     //se hace la muta con los index
                     auto ui = population[r1].values[j]+F_*(population[r2].values[j]-population[r3].values[j]);
-                    child.push_back(ui);
+                    child.push_back(ui); 
+                    distributionR(random);
+                    Crrandom = distributionR(random); 
+                    j++;
+                    initj=j;
                     
-                }else{
-                    //agregas al hijo el padre
-                    child.push_back(population[i].values[j]);
+                }
+                for(size_t j1 = initj; j1 < dimension_ ;j1++){ 
+                    child.push_back(population[i].values[j1]);
                 }
                 
             }
+
             double fitX=rosenbrockFunction(population[i].values);
             double fitU=rosenbrockFunction(child);
             if(fitU<=fitX){
                 population[i].values=child;
                 population[i].fitness=fitU;
-            }
-            fitX=rosenbrockFunction(population[i].values);
-            if(fitX<best){
-                best = fitX;
-                bestx.values=population[i].values;
-                bestx.fitness=fitX;
-            }
-            
+            } 
         }
         if(best==0){
             break;
         }
-        std::cout<<"Best: "<<best<<std::endl;
+        
+        for (size_t w = 0; w < population_size_; w++){
+            double fitX=rosenbrockFunction(population[w].values);
+            if(fitX<best){
+                best = fitX;
+                bestx.values=population[w].values;
+                bestx.fitness=fitX;
+            }
+        }
         for(auto x:bestx.values){
             std::cout<<x<<", ";
         }
--- Evolutionary Algorithms/main.cpp
+++ Evolutionary Algorithms/main.cpp
@@ -10,17 +10,22 @@ int main() {
 
     int population_size = 50;
     int dimension = 10;
-    int generation_max=100;
+    int generation_max=500;
     double lower_bound=-10;
     double upper_bound=10;
     double F=0.6;
     double Cr=0.9;
+
+    const int bin=0;
+    const int expo=1;
+    const int rand=0;
+    const int best=1;
     
     DifferentialEvolution de(population_size, dimension,generation_max,lower_bound,upper_bound,F,Cr);
     
-    de.runEvolution("rand/1/bin");
-    de.runEvolution("rand/1/exp");
-    de.runEvolution("best/1/bin");
-    de.runEvolution("best/1/exp");
+    //de.runEvolution(rand,bin);//de.runEvolution("rand/1/bin");
+    //de.runEvolution(best,bin);//de.runEvolution("best/1/bin");
+    de.runEvolution(rand,expo);//de.runEvolution("rand/1/exp");
+    //de.runEvolution("best/1/exp");
     return 0;
 }
