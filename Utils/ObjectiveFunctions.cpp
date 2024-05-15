3d4495579eed7f1344277dd03304c2e98dcc479d
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2024-05-15 17:55:17 -0600

    Utils, objective functions for benchmark

---
+++ Utils/ObjectiveFunctions.cpp
@@ -0,0 +1,48 @@
+#include "ObjectiveFunctions.h"
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+double rosenbrockFunction(const std::vector<double>& individual) {
+    double rosenbrock = 0.0;
+    for (size_t i = 0; i < individual.size() - 1; ++i) {
+        rosenbrock += 100 * std::pow((individual[i+1] - std::pow(individual[i], 2)), 2) + std::pow((1 - individual[i]), 2);
+    }
+    return rosenbrock;
+}
+
+double ackleyFunction(const std::vector<double>& individual) {
+    double sum1 = 0.0;
+    double sum2 = 0.0;
+    for (auto x_i : individual) {
+        sum1 += std::pow(x_i, 2);
+        sum2 += std::cos(2 * M_PI * x_i);
+    }
+    double ackley = -20 * std::exp(-0.2 * std::sqrt(sum1 /  individual.size())) - std::exp(sum2 / individual.size()) + 20 + M_E;
+    return ackley;
+}
+double GriewankFunction(const std::vector<double>& individual){
+    double propor=1.0/4000.0;
+    double sum = 0.0;
+    double mult = 0.0;
+    int i=1;
+    for (size_t i = 0; i < individual.size(); i++)
+    {
+        auto x_i=individual[i];
+        sum += std::pow(x_i, 2);
+        mult *= std::cos(x_i / sqrt(i+1));
+    }
+    
+    double griewank = (propor*sum)-(mult)+1;
+
+    return griewank;
+
+}
+double RastriginFunction(const std::vector<double>& individual){
+    double sum = 0.0;
+    for (auto x_i : individual) {
+        sum += (std::pow(x_i, 2))-(10*(std::cos(2*M_PI*x_i)));
+    }
+    double rastrigin = sum+(10*individual.size());
+    return rastrigin;
+}
---
+++ Utils/ObjectiveFunctions.h
@@ -0,0 +1,10 @@
+#include <numeric>
+#include <vector>
+
+#define M_PI 3.14159265358979323846
+#define M_E 2.71828182845904523536
+
+double rosenbrockFunction(const std::vector<double>& individual);
+double ackleyFunction(const std::vector<double>& individual);
+double GriewankFunction(const std::vector<double>& individual);
+double RastriginFunction(const std::vector<double>& individual);
